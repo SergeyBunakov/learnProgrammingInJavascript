717e49154d642bc038a517b1b95b603222520a17
sergbanderlogin <sergbanderlogin@gmail.com>
2020-09-20 20:29:40 +0300

    rename script.js to representation.js and create model.js

---
+++ 08-building-the-application/representation.js
@@ -0,0 +1,26 @@
+const view = {
+    displayMassage: function (msg) {
+        let messageArea = document.getElementById('messageArea');
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute('class', 'hit');
+    },
+    displayMiss: function (location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute('class', 'miss');
+    }
+};
+let msg = "Tap tap, is this thing on?";
+
+view.displayMassage(msg);
+
+view.displayMiss('00');
+view.displayHit('01');
+view.displayMiss('02');
+view.displayHit('33');
+view.displayMiss('36');
+view.displayHit('13');
+view.displayMiss('22');
+view.displayHit('55');
